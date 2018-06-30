9c402b203badce237f74fd7333884d1473df7cf1
analben <analmanakiwala@gmail.com>
2018-06-29 21:30:12 -0400

    Checkpoint-audioplayback: completed

--- bloc-jams-react/src/components/Album.js
+++ bloc-jams-react/src/components/Album.js
@@ -75,7 +75,7 @@ class Album extends Component {
                <tbody>
                {this.state.album.songs.map((song,index) => 
                	    
-               	 <tr className="song" key={index} onClick={() =>this.handleSongClick(song)}	>
+               	 <tr className="song" key={index} onClick={() => this.handleSongClick(song)}	>
                		
                )}
                
